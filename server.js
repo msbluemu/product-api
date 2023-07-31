baad47a4a1cad1cf9517ff392595b6151ea44c29
Qianyun Mu <msbluemu@gmail.com>
2023-07-31 21:09:39 +1000

    modify server.js

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
-// JSON Server module
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-
-// Make sure to use the default middleware
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-// Add this before server.use(router)
 server.use(router);
-// Listen to port
-server.listen(3000, () => {
- console.log("JSON Server is running");
-});
 
-// Export the Server API
-module.exports = server;
+server.listen(port);
